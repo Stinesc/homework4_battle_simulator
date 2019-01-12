4942bce70984e473302cbffeee5e0f38815f13d4
nesc <limarevigor@gmail.com>
2019-01-12 08:15:58 +0200

    добавил тесты c моками

--- tests/units/test_soldier.py
+++ tests/units/test_soldier.py
@@ -1,5 +1,7 @@
 import unittest
+from unittest.mock import patch
 from models.units.soldier import Soldier
+from models.units.unit_base_mixin import UnitBaseMixin
 
 
 class TestSoldier(unittest.TestCase):
@@ -21,3 +23,15 @@ class TestSoldier(unittest.TestCase):
         soldier = Soldier()
         soldier.take_damage(1)
         self.assertEqual(soldier.health, 99)
+
+    @patch('models.units.soldier.randint')
+    def test_attack_success(self, mock_randint):
+        mock_randint.return_value = 50
+        soldier = Soldier()
+        self.assertEqual(soldier.attack_success(), 0.5)
+
+    @patch('models.units.unit_base_mixin.randint')
+    def test_recharge(self, mock_randint):
+        mock_randint.return_value = 100
+        soldier = Soldier()
+        self.assertEqual(soldier.recharge, mock_randint.return_value)
--- tests/units/test_vehicle.py
+++ tests/units/test_vehicle.py
@@ -1,14 +1,22 @@
 import unittest
+from unittest.mock import patch
 from models.units.vehicle import Vehicle
 from models.units.soldier import Soldier
+from models.units.unit_base_mixin import UnitBaseMixin
 
 
 class TestVehicle(unittest.TestCase):
 
     def test_check_active_active(self):
-        squad = Vehicle(units=[Soldier()])
-        self.assertTrue(squad.check_active())
+        vehicle = Vehicle(operators=[Soldier()])
+        self.assertTrue(vehicle.check_active())
 
     def test_check_active_not_active(self):
-        squad = Vehicle(units=list())
-        self.assertFalse(squad.check_active())
+        vehicle = Vehicle(operators=list())
+        self.assertFalse(vehicle.check_active())
+
+    @patch('models.units.unit_base_mixin.randint')
+    def test_recharge(self, mock_randint):
+        mock_randint.return_value = 2000
+        vehicle = Vehicle(operators=list())
+        self.assertEqual(vehicle.recharge, mock_randint.return_value)
